fd21f9855b4014eb505ca25a19a47721dce556da
Olga <olga.kim.box@gmail.com>
2021-11-04 23:11:58 +0300

    2 screens

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screen1 from './src/screens/Screen1';
+import Screen2 from './src/screens/Screen2';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator>
+       <Stack.Screen name = 'Экран 2' component = {Screen1}/>
+       <Stack.Screen name = 'Экран 1' component = {Screen2}/>
+       
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Screen2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+
+
+export default function NewScreen({navigation}) {
+  return (
+   <View style= {{flex: 1, backgroundColor: 'pink'}}>  
+   </View>  
+  );
+}
